522b3ee728d05103675719618c6f6016217987c0
m3chas <joazrivera@gmail.com>
2017-08-07 11:13:38 -0600

    Added a new Django App called API

--- carapi/carapi/settings.py
+++ carapi/carapi/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework', # Django Rest_Framework to build our Car API
 ]
 
 MIDDLEWARE = [
